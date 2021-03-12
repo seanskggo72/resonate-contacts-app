b091ef76326c9f33a24158a7a8ba080693730e33
seanskggo <65769889+seanskggo@users.noreply.github.com>
2021-03-12 16:16:06 +1100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Card from 'react-bootstrap/Card';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Jumbotron fluid>
+        <Container>
+          <h1>Contacts</h1>
+        </Container>
+      </Jumbotron>
       <Card border="info" style={{ width: '18rem' }}>
         <Card.Header>Header</Card.Header>
         <Card.Body>
