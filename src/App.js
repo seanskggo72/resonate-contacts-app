d5c7c15415920e1011d0cecb234a619d20c3c9e3
seanskggo <65769889+seanskggo@users.noreply.github.com>
2021-03-12 18:24:39 +1100

    delete functionality complete

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Card from 'react-bootstrap/Card';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import sample from './sample'
 
 const Contacts = () => {
   const [contacts, set_contacts] = useState(sample);
-  console.log(contacts);
+  const delete_contact = (id) => {
+    contacts.splice(id, 1);
+    let temp = [...contacts];
+    set_contacts(temp);
+  }
   return (
-    <Accordion defaultActiveKey="0">
+    <Accordion defaultActiveKey="">
       {contacts.map((x, index) => {
-        console.log(index)
         return (
           <Card border="info" className='Each' key={index}>
             <Accordion.Toggle as={Card.Header} eventKey={index.toString()}>
@@ -22,15 +26,12 @@ const Contacts = () => {
             <Accordion.Collapse eventKey={index.toString()}>
               <Card.Body>
                 <Card.Title>Phone: {x.phone}</Card.Title>
-                <Card.Text>
-                  Email: {x.email}
-                </Card.Text>
-                <Card.Text>
-                  Company: {x.company.name}
-                </Card.Text>
-                <Card.Text>
-                  Website: {x.website}
-                </Card.Text>
+                <Card.Text> Email: {x.email} </Card.Text>
+                <Card.Text> Company: {x.company.name} </Card.Text>
+                <Card.Text> Website: {x.website} </Card.Text>
+                <Button variant="outline-danger" onClick={() => {delete_contact(index)}}>
+                  Delete
+                </Button>
               </Card.Body>
             </Accordion.Collapse>
           </Card>
