dc72df704cf53c7801794dac4dbfb0eb737fe6e1
seanskggo <65769889+seanskggo@users.noreply.github.com>
2021-03-12 16:50:19 +1100

    flexbox responsive

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
+import { useState } from 'react';
 import './App.css';
 import Card from 'react-bootstrap/Card';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const Contacts = () => {
+  const [contacts, set_contacts] = useState({});
+  console.log(contacts);
+  return (
+    <div className='Contact_layout'>
+      <div className='Sep'>
+        <Card border="info" className='Each'>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Info Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk
+              of the card's content.
+          </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className='Sep'>
+        <Card border="info" className='Each'>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Info Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk
+              of the card's content.
+          </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className='Sep'>
+        <Card border="info" className='Each'>
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>Info Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk
+              of the card's content.
+          </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <Jumbotron fluid>
+      <Jumbotron fluid className='Banner'>
         <Container>
           <h1>Contacts</h1>
         </Container>
       </Jumbotron>
-      <Card border="info" style={{ width: '18rem' }}>
-        <Card.Header>Header</Card.Header>
-        <Card.Body>
-          <Card.Title>Info Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk
-            of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <Contacts />
     </div>
   );
 }
